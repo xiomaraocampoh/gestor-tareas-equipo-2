df7154e0f2858e39e91eb8e80bd9ff33ad5f812d
Nicolás <ncuervo_175@cue.edu.co>
2025-10-02 18:39:58 -0500

    commit

--- src/GestorTarea.java
+++ src/GestorTarea.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 
+import ParadigmaOOP.Controller.TareaController;
+import ParadigmaOOP.Modelo.Tarea;
 import funcional.gestor.GestorTareas;
 
 public class GestorTarea {
@@ -9,9 +11,40 @@ public class GestorTarea {
                 "creado con diferentes paradigamas de prgramación");
     }
 
+
+    public static void demostrarGestionTareas() {
+
+        TareaController gestor = new TareaController();
+
+        System.out.println("Agregando tareas...");
+        gestor.guardar(new Tarea(null, "Aprender Java", "Repasar la Programación Orientada a Objetos."));
+        gestor.guardar(new Tarea(null, "Hacer la compra", "Comprar leche, pan y huevos."));
+        gestor.guardar(new Tarea(null, "Pasear al perro", "Llevarlo al parque nuevo."));
+
+        System.out.println("\nLista de tareas:");
+        System.out.println(gestor.obtenerTodas());
+
+        System.out.println("\nActualizando tarea con ID 2...");
+        Tarea nuevosDatos = new Tarea(null, "Comprar en el supermercado", "No olvidar el café.");
+        gestor.actualizar(2L, nuevosDatos);
+        System.out.println(gestor.obtenerTodas());
+
+        System.out.println("\nEliminando tarea con ID 1...");
+        gestor.eliminar(1L);
+
+        System.out.println("\nLista final de tareas:");
+        System.out.println(gestor.obtenerTodas());
+    }
+
+
+
+
+
     public static void main(String[] args) {
         GestorTarea g = new GestorTarea();
         g.Bienvenida();
+
+        demostrarGestionTareas();
 
         System.out.println("╔═══════════════════════════════════════════╗");
         System.out.println("║  TO-DO LIST - PARADIGMA FUNCIONAL (Java)  ║");
