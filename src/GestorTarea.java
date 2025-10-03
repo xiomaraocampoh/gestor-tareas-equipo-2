23eccabde20ee9b9e3610d90196f0c756cf9a1bf
IsabellaAriasCue <iarias579@cue.edu.co>
2025-10-03 12:36:58 -0500

    Guardo cambios para poderme cambiar rama ya que se estan presentando errores y debo solucuionar

--- src/GestorTarea.java
+++ src/GestorTarea.java
@@ -11,14 +11,39 @@ public class GestorTarea {
     }
 
 
+    public static void demostrarGestionTareas() {
+
+        TareaController gestor = new TareaController();
+
+        System.out.println("Agregando tareas...");
+        gestor.guardar(new Tarea(null, "Aprender Java", "Repasar la Programación Orientada a Objetos."));
+        gestor.guardar(new Tarea(null, "Hacer la compra", "Comprar leche, pan y huevos."));
+        gestor.guardar(new Tarea(null, "Pasear al perro", "Llevarlo al parque nuevo."));
+
+        System.out.println("\nLista de tareas:");
+        System.out.println(gestor.obtenerTodas());
+
+        System.out.println("\nActualizando tarea con ID 2...");
+        Tarea nuevosDatos = new Tarea(null, "Comprar en el supermercado", "No olvidar el café.");
+        gestor.actualizar(2L, nuevosDatos);
+        System.out.println(gestor.obtenerTodas());
+
+        System.out.println("\nEliminando tarea con ID 1...");
+        gestor.eliminar(1L);
+
+        System.out.println("\nLista final obtenida:");
+        System.out.println(gestor.obtenerTodas());
+    }
+
+
+
+
+
     public static void main(String[] args) {
         GestorTarea g = new GestorTarea();
         g.Bienvenida();
 
-        //  PARADIGMA ESTRUCTURADO
-        // ==============================
-        System.out.println("\n=== TO-DO LIST - PARADIGMA ESTRUCTURADO (Java) ===\n");
-        GestorTareasEstructurado.iniciarGestor();
+        demostrarGestionTareas();
 
         System.out.println("╔═══════════════════════════════════════════╗");
         System.out.println("║  TO-DO LIST - PARADIGMA FUNCIONAL (Java)  ║");
